12d13302927b48057ef8f20cc449be6aaea3eb09
james yang <yanghongday369@gmail.com>
2020-10-28 13:26:29 +0800

    Update pages.py

--- router/pages.py
+++ router/pages.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from .generate import app, templates
 from config import *
