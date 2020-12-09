c16f6e457e285bc035a4ec61f6b8aa297c3002b8
james yang <yanghongday369@gmail.com>
2020-12-10 01:35:30 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 from config import port
 from router import app
+import argparse
 import uvicorn
 
+parser = argparse.ArgumentParser(description='Config Options')
+parser.add_argument('-E', '--env', help='custom PATH of dotenv file')
+args = parser.parse_args()
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host="127.0.0.1", port=port, log_level="info")
