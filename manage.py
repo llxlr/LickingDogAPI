49960f48ebe8ab9fc24b4afee66d424ab54a8181
james yang <yanghongday369@gmail.com>
2021-02-07 15:42:12 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from config import port
 from router import app, templates
 import uvicorn
+import os
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host="127.0.0.1", port=port, log_level="info")
+    uvicorn.run(app=app, host=os.getenv('HOST'), port=os.getenv('PORT'), log_level="info")
     # uvicorn manage:app --host 127.0.0.1 --port 8001
     # gunicorn manage:app -w 4 -k uvicorn.workers.UvicornWorker
