64eaca9e02fd3bc1edcfc4b51ebb1bf3fc35224d
llxlr <yanghongday369@gmail.com>
2021-06-18 20:04:22 +0800

    ok

--- app/utils/spider.py
+++ app/utils/spider.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from requests.exceptions import ConnectionError, ConnectTimeout
 import requests
-from .. import config
+import config
 
 
 def get(url, params=None, headers: dict = None):
